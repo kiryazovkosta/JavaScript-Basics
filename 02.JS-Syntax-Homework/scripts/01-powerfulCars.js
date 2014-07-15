e294bfc48043e2871f398399fc418e7b89672e2f
Kosta Kiryazov <kosta.kiryazov@gmail.com>
2014-07-15 16:53:31 +0300

    Start task from a homework at JavaScript Syntax

---
+++ 02.JS-Syntax-Homework/scripts/01-powerfulCars.js
@@ -0,0 +1,8 @@
+﻿function convertKWtoHP(value) {
+    var hp = value / 0.746;
+    return hp.toFixed(2);
+}
+
+console.log(convertKWtoHP(75));
+console.log(convertKWtoHP(150));
+console.log(convertKWtoHP(1000));
---
+++ 02.JS-Syntax-Homework/scripts/02-roundingNumbers.js
@@ -0,0 +1,8 @@
+﻿function roundNumber(value) {
+    console.log("Round: " + Math.round(value));
+    console.log("Floor: " + Math.floor(value));
+}
+
+roundNumber(22.7);
+roundNumber(12.3);
+roundNumber(58.7);
