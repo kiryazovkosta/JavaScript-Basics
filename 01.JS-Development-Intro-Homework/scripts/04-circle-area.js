4b791f40ac416b4422ba36cd33035ac24423a630
Kosta Kiryazov <kosta.kiryazov@gmail.com>
2014-07-15 16:10:01 +0300

    First homework without tasks for the champions

---
+++ 01.JS-Development-Intro-Homework/scripts/04-circle-area.js
@@ -0,0 +1,13 @@
+﻿function calcCircleArea(r) {
+    var area = Math.PI * r * r;
+    return area;
+}
+
+function calculateAreas() {
+    var text7 = document.getElementById('a7');
+    text7.innerHTML += calcCircleArea(7);
+    var text1_5 = document.getElementById('a1-5');
+    text1_5.innerHTML += calcCircleArea(1.5);
+    var text20 = document.getElementById('a20');
+    text20.innerHTML += calcCircleArea(20);
+}
