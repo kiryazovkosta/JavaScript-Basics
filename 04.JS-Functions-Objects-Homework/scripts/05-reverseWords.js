98125e5d69b47b22a514e3f81001738b038acb4c
Kosta Kiryazov <kosta.kiryazov@gmail.com>
2014-07-24 12:21:20 +0300

    another tasks become ready

--- 04.JS-Functions-Objects-Homework/scripts/05-reverseWords.js
+++ 04.JS-Functions-Objects-Homework/scripts/05-reverseWords.js
@@ -1,1 +1,22 @@
-﻿
+﻿// Write a JavaScript function reverseWordsInString(str) to reverse the characters of every word in the string 
+// but leaves the words in the same order. Words are considered to be sequences of characters separated by spaces. 
+// Write a JavaScript program reverseWords.js that prints on the console
+
+function reverseWordsInString(str) {
+    var words = str.split(' ');
+    words = words.filter(function (n) { return n !== ''; });
+    for (var i = 0; i < words.length; i++) {
+        var reverce = '';
+        for (var j = words[i].length - 1; j >= 0; j--) {
+            reverce += words[i].charAt(j);
+        }
+
+        words[i] = reverce;
+    }
+
+    return words;
+}
+
+console.log(reverseWordsInString('Hello, how are you.').join(' '));
+console.log(reverseWordsInString('Life is pretty good, isn’t it?').join(' '));
+console.log(reverseWordsInString('Write a JavaScript function reverseWordsInString(str) to reverse the characters of every word in the string but leaves the words in the same order. Words are considered to be sequences of characters separated by spaces. Write a JavaScript program reverseWords.js that prints on the console').join(' '));
--- 04.JS-Functions-Objects-Homework/scripts/07-youngestPerson.js
+++ 04.JS-Functions-Objects-Homework/scripts/07-youngestPerson.js
@@ -1,1 +1,13 @@
-﻿
+﻿// Write a JavaScript function findYoungestPerson(persons) that accepts as parameter an array of persons, 
+// finds the youngest person and returns his full name. Write a JS program youngestPerson.js to execute 
+// your function for the below examples and print the result at the console.
+
+function findYoungestPerson(persons) {
+
+}
+
+var persons = [
+  { firstname: 'George', lastname: 'Kolev', age: 32 },
+  { firstname: 'Bay', lastname: 'Ivan', age: 81 },
+  { firstname: 'Baba', lastname: 'Ginka', age: 40 }]
+findYoungestPerson(persons);
