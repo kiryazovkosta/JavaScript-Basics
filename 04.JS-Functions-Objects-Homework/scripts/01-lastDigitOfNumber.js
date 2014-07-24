9e746e3220c542a6e515114a4583542c159ac767
Kosta Kiryazov <kosta.kiryazov@gmail.com>
2014-07-24 11:51:52 +0300

    first four tasks

--- 04.JS-Functions-Objects-Homework/scripts/01-lastDigitOfNumber.js
+++ 04.JS-Functions-Objects-Homework/scripts/01-lastDigitOfNumber.js
@@ -1,1 +1,38 @@
-﻿
+﻿// Write a JavaScript function lastDigitAsText(number) that returns the last digit of given integer as an English word. 
+// Write a JS program lastDigitOfNumber.js that invokes your function with the sample input data below and prints the 
+// output at the console.
+
+function lastDigitAsText(number) {
+
+    var number = Number(number);
+    var digit = Math.abs(number) % 10;
+    return DigitAsText(digit);
+}
+
+function DigitAsText(digit) {
+    if (typeof(digit) !== 'number') {
+        return 'Invalid';
+    }
+
+    var text = '';
+    switch (digit) {
+        case 0: text = 'Zero'; break;
+        case 1: text = 'One'; break;
+        case 2: text = 'Two'; break;
+        case 3: text = 'Three'; break;
+        case 4: text = 'Four'; break;
+        case 5: text = 'Five'; break;
+        case 6: text = 'Six'; break;
+        case 7: text = 'Seven'; break;
+        case 8: text = 'Eight'; break;
+        case 9: text = 'Nine'; break;
+        default: text = 'Incorrect';
+    }
+
+    return text;
+}
+
+console.log(lastDigitAsText(6));
+console.log(lastDigitAsText(-55));
+console.log(lastDigitAsText(133));
+console.log(lastDigitAsText(14567));
--- 04.JS-Functions-Objects-Homework/scripts/02-nthDigitOfNumber.js
+++ 04.JS-Functions-Objects-Homework/scripts/02-nthDigitOfNumber.js
@@ -1,1 +1,8 @@
-﻿
+﻿// Write a JavaScript function findNthDigit(arr) that accepts as a parameter an array of two numbers num and n 
+// and returns the n-th digit of given decimal number num counted from right to left. Return undefined when the 
+// number does not have n-th digit. Write a JS program nthDigitOfNumber.js that invokes your function with the 
+// sample input data below and prints the output at the console. 
+
+function findNthDigit(arr) {
+
+}
